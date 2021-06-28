9f6085e05aa24c4b0ab2f6f087c0f0abdcbcfe21
ThuThao-Jul <84497355+ThuThao-Jul@users.noreply.github.com>
2021-06-28 22:58:18 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,173 @@
+Assignment 2
+// console.log("area of a circle")
+// var pi=3.14
+// console.log(typeof pi)
+// var radius=7
+// console.log(typeof radius)
+// var area=pi*radius**2
+// console.log("The area of a circle given the radius", radius, "is",area, "m2") 
+
+Assignment 3
+// console.log(34==10+24)
+// console.log(34=="10"+"24")
+// console.log("Hello"+""+"2021"=="Hello 2021")
+// console.log(7==1+2*3)
+// console.log((1+3)**2==16)
+// console.log(1/0==0)
+// console.log(6%2==0)
+// console.log(5.5%2==1.5)
+// console.log(Number("123")==123)
+// console.log(typeof(1+""))
+// console.log(5 == "5")
+// console.log(5 === "5")
+// console.log(8 != 8.0)
+// console.log(8 !== 8.0)
+// console.log("true" === true)
+// console.log(4 <= 4.0)
+// console.log(7 >= 7)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(true || true)
+// console.log(false || true)
+// console.log(!true)
+// console.log(!false)
+// console.log(false && (true || true))
+// console.log(false && true || true)
+
+
+Assignment 4
+// var x=24
+// if (x>0){
+//     console.log("x is a positive number")
+// } else if(x<0){
+//     console.log("x is a negative number")
+// } else {
+//     console.log("x is 0")
+// }
+// var a=46
+// b=24
+// c=12
+// max=a
+// min=a
+// if (max>=b){
+//     max=max
+// } else {max=b}
+// if (max>=c){
+//     max=max
+// } else {max=c}
+
+// if (min>=b){
+//     min=b
+// } else {min=min}
+// if (min>=c) {
+//     min=c
+// } else {min=min}
+// console.log(`${max} is the largest number, ${min} is the smallest number between ${a}, ${b}, ${c}`)
+
+
+
+Assignment 5
+// for(a=2, b=8; a<=8; a++){
+//     console.log(a);
+// }
+
+// for (a=9, b=4; a>=b; a--){
+//     console.log(a);
+// }
+
+// var x=1
+// y=10
+// sum=x
+// n=x+1
+
+// while (n<=y){
+//     sum=sum+n;
+//     n=n+1;
+// }
+// console.log(`Sum of numbers from ${x} to ${y} is ${sum}`)
+
+
+
+// var x=6294
+// a=0
+// b=0
+// c=0
+// d=0
+// sum=0
+// a=Math.floor(x/1000)
+// b=Math.floor((x-a*1000)/100)
+// c=Math.floor((x-a*1000-b*100)/10)
+// d=Math.floor(x-a*1000-b*100-c*10)
+// console.log(sum=a+b+c+d)
+
+
+
+Assignmaent 6
+// function FirstFunction(){
+//     console.log("Wow, it worked!")
+// }
+
+// function SecondFunction(){
+//     return "my favorite movie"
+// }
+// console.log(SecondFunction())
+
+
+
+// function ThirdFunction("Thao"){
+//     return `xscsccw ${ThirdFunction} wefef`
+// }
+// console.log(ThirdFunction())
+
+
+// function SerieofNumbers(a,b){
+//     for(a=3,b=7; a<=b; a++){
+//         console.log(a);
+//     }
+// }
+
+
+
+Assignment 7
+// var a=111
+// b=999
+// temp=0
+// console.log(`Before: a=${a} and b=${b}`)
+// temp=a
+// a=b
+// b=temp
+// console.log(`After: a=${a} and b=${b}`)
+
+
+
+// var a=241
+// b=356 
+// a=a+b //old a and old b //
+// console.log(a)
+// b=a-b //new a and old b //
+// console.log(b)
+// a=a-b //new new a and new b //
+// console.log(`After: a=${a} and b=${b}`)
+
+
+
+Assignment 8
+// console.log(Math.floor(Math.random()*2));
+// console.log(Math.floor(Math.random()*11));
+
+
+
+Assignment 9
+// function play(){
+//     const randomNumber = Math.floor(Math.random()*2);
+//     if (randomNumber == 0) {
+//         console.log("Yes");
+//     } else {
+//         console.log("No")}
+// }
+
+// console.log("What's your name?")
+// play()
+
+
+
